19dc7f4c8376aca57371abf6ec83b19e8f89d55f
Albin <albinkuttu8785@gmail.com>
2026-06-03 14:33:07 +0530

    requirements

--- backend/employees/tests.py
+++ backend/employees/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Employee
 
-# Create your tests here.
+
+class EmployeeModelTest(TestCase):
+
+    def test_employee_creation(self):
+
+        employee = Employee.objects.create(
+            name="Albin",
+            role="Backend Developer"
+        )
+
+        self.assertEqual(
+            employee.name,
+            "Albin"
+        )
